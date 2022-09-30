81a9e9d18f81e76a234a95bacff58dd6a9b1eab8
MikhailSumarokov <ykpon1995@mail.ru>
2022-09-30 14:14:05 +0500

    0.23
    Add Script Camera

--- Fly out/Assets/Scripts/Camera.cs
+++ Fly out/Assets/Scripts/Camera.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    [SerializeField] private Vector3 offSetPosition = new Vector3(0, 3f, -4.5f);
-    private Transform _targetFollow;
+    [SerializeField] private float damping = 1;
+    [SerializeField] private Vector3 offset = new Vector3(0, 3f, -4.5f);
+    private GameObject _target;
 
     private void LateUpdate()
     {
-        if (_targetFollow != null)
-        {
-            transform.position = _targetFollow.position + offSetPosition;
-        }
+        if (_target == null) return;
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = _target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+        //Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = _target.transform.position + (_target.transform.rotation * offset);
+        transform.LookAt(_target.transform);
     }
 
-    public void SetTargetFollow(GameObject target) => _targetFollow = target.transform;
+    public void SetTarget(GameObject target) => _target = target;
 }
